05b20d386b41b6d4070a91ec9f83c1094a213599
Sunny <sundeepkmanik@gmail.com>
2018-03-12 21:47:37 -0700

    added randomizer function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,15 @@
+// global variables
+
+var wordBank = ["strudel", "baklava", "cannoli", "pie", "macaron", "pretzel", "doughnut"];
+
+var storedWord = wordBank[(Math.random()*wordBank.length)|0];
+console.log('item:', storedWord);
+
+// when user presses key
+
 document.onkeyup = function (event) {
 
-    //var wordBank = ["strudel", "baklava", "cannoli", "pie", "macaron", "pretzel", "doughnut"];
-
-    var storedWord = "strudel";
+    // this was just to test the conditions below: var storedWord = "strudel";
 
     var userLetter = String.fromCharCode(event.which).toLowerCase();
 
