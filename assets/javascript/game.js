73d3c04092ef01714827a572dd392620189a9815
Sunny <sundeepkmanik@gmail.com>
2018-03-13 00:44:29 -0700

    display user key in letter bank

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,16 @@
 // ===== global variables
 
-// array containing words
+// stores words in array
 
 var wordBank = ["strudel", "baklava", "cannoli", "tart", "macaron", "pretzel", "doughnut"];
 
-// computer selects from array
+// selects from array
 
-var storedWord = wordBank[(Math.random() * wordBank.length)|0];
+var storedWord = wordBank[(Math.random() * wordBank.length) | 0];
 
 console.log("item:", storedWord);
 
-// computer counts string length and displays spaces on screen
+// counts string length and displays spaces on screen
 
 var blankWord = "_ ".repeat(storedWord.length);
 
@@ -20,13 +20,22 @@ var wordDisplay = document.getElementById("word-display");
 
 wordDisplay.textContent = blankWord;
 
-// ===== user presses key
+// ===== lets user presses key
 
 document.onkeyup = function (event) {
 
-    // this was just to test the conditions below: var storedWord = "strudel";
-
     var userLetter = String.fromCharCode(event.which).toLowerCase();
+
+    // stores user key in bank
+
+    var letterBank = document.getElementById("letter-bank");
+    var usedLetter = document.createElement("span");
+
+    usedLetter.setAttribute("id", "used-letter");
+
+    usedLetter.textContent = userLetter + " ";
+
+    letterBank.appendChild(usedLetter);
 
     if (storedWord.includes(userLetter)) {
 
